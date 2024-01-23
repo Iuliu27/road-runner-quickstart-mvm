c13527892c07369344bd1ee2d09c4f8ad5fc8e19
Iuliu27 <133576069+Iuliu27@users.noreply.github.com>
2024-01-23 19:16:50 +0200

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoTest.java
@@ -6,11 +6,14 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+
 
 import org.checkerframework.checker.signature.qual.FieldDescriptorForPrimitiveOrArrayInUnnamedPackage;
 import org.firstinspires.ftc.teamcode.HardwareMapping;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.PoseTransfer;
+import org.opencv.core.Mat;
 
 @Autonomous(name="VASILEEEEE")
 public class AutoTest extends LinearOpMode { //ok
@@ -18,13 +21,15 @@ public class AutoTest extends LinearOpMode { //ok
     HardwareMapping.Intake intake = Robot.new Intake();
     HardwareMapping.Outtake outtake = Robot.new Outtake();
     private MecanumDrive drive;
-    private Pose2d cPose;
+    private Pose2d cPose=new Pose2d(-34.5,-58, Math.toRadians(90));
 
     @Override
     public void runOpMode() throws InterruptedException {
         Robot.init(hardwareMap);
-        Pose2d firstPose=new Pose2d(-34.5,-58,90);
-        Pose2d RightLane=new Pose2d(-29,-33,60);
+        Pose2d firstPose=new Pose2d(-34.5,-58,Math.toRadians(60));
+        Pose2d RightLane=new Pose2d(-29,-33,Math.toRadians(60));
+        //TrajectoryActionBuilder builder = mecanumDrive.actionBuilder(cPose);
+
 
         cPose=firstPose;
 
@@ -35,12 +40,13 @@ public class AutoTest extends LinearOpMode { //ok
 
         waitForStart();
 
+        Actions.runBlocking(new SequentialAction(
+                TrajRightLane
+        ));
+
         while(opModeIsActive() && !isStopRequested()){
-            Actions.runBlocking(new SequentialAction(
-                    TrajRightLane
-            ));
             Pose2d currentPose = drive.pose;
-            PoseTransfer.currentPose = currentPose;
+             PoseTransfer.currentPose = currentPose;
             telemetry.addData("x :",drive.pose.position.x);
             telemetry.addData("y :",drive.pose.position.y);
             telemetry.addData("Current Pose :",cPose);
